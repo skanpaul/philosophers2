b70e8fd60280a63a160bd77e57bd13fab7da3f10
Sorakannn SurfacePro7 <sorakann.ki@gmail.com>
2022-03-30 15:51:58 +0200

    init tv_start for all philosopher structure

--- utils_3.c
+++ utils_3.c
@@ -6,16 +6,18 @@
 int start_thread(t_data *d)
 {
 	int i;
-
+	t_timeval start;
 	t_philo *p_list;
 	t_philo *p;
 
+	gettimeofday(&start, NULL);
 	p_list = d->philo_list;
 
 	i = 0;
 	while (i < d->max_philo)
 	{
 		p = &p_list[i];
+		p[i].tv_start = start;
 		// ------------------------------------------------------------------
 		if (pthread_create(&p->th_id, NULL, &philo_activity, (void *)p) != 0)
 			return (print_error(TYPE_ERR_THD_CREATE));
