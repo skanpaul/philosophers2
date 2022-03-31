13955b50b5fb054fe81270cb864de643310489fc
sorakann iMac <ski@student.42lausanne.com>
2022-03-31 16:48:52 +0200

    utilisation de lock(mtx_message) pour print_mutex()

--- print_status.c
+++ print_status.c
@@ -52,11 +52,10 @@ void print_mutex(t_philo *p, int64_t stamp_us, char *msg)
 
 	stamp_ms= (stamp_us - p->stamp_us_start) / 1000;
 
-	// pthread_mutex_lock(&p->d->mtx_message);
-
+	pthread_mutex_lock(&p->d->mtx_message);
 	printf("op:%5d | %15lld[ms] %d %s", p->d->operation, stamp_ms, p->id, msg);
 	p->d->operation++;
-	// pthread_mutex_unlock(&p->d->mtx_message);
+	pthread_mutex_unlock(&p->d->mtx_message);
 }
 /* ************************************************************************** */
 
