957584753da03e134be93ce55a092dad4c142d15
sorakann iMac <ski@student.42lausanne.com>
2022-03-31 11:08:12 +0200

    ajout: usleep(50) dans check_life()

--- start_thread.c
+++ start_thread.c
@@ -22,7 +22,7 @@ int start_thread(t_data *d)
 		if (pthread_create(&p->th_id, NULL, &philo_activity, (void *)p) != 0)
 			return (print_error(TYPE_ERR_THD_CREATE));
 		// ------------------------------------------------------------------
-		usleep(1000);
+		usleep(2000);
 		// ------------------------------------------------------------------
 		// DETACH THREAD IF NECESSARY
 		// ------------------------------------------------------------------
