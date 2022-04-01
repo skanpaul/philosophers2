0a813bb0cf5668ad279644d1ef0d1060b8c923c7
sorakann iMac <ski@student.42lausanne.com>
2022-04-01 11:37:14 +0200

    correcion affichage action sans nombre d-operation

--- print_status_2.c
+++ print_status_2.c
@@ -20,8 +20,16 @@ void	print_max_meal(t_philo *p)
 	stamp_us_now = get_stamp_us_now();
 	stamp_us_max_meal = (stamp_us_now - p->stamp_us_start) / 1000;
 	pthread_mutex_lock(&p->d->mtx_message);
-	printf("           %15lld[ms] EVERY ONE HAVE EATEN %d MEAL(S)\n",
-		stamp_us_max_meal, p->max_meal);
+	if (INFO == 1)
+	{
+		printf("           %15lld[ms] EVERY ONE HAVE EATEN %d MEAL(S)\n",
+			stamp_us_max_meal, p->max_meal);
+	}
+	else
+	{
+		printf("%15lld[ms] EVERY ONE HAVE EATEN %d MEAL(S)\n",
+			stamp_us_max_meal, p->max_meal);
+	}
 	pthread_mutex_unlock(&p->d->mtx_message);
 }
 
@@ -32,7 +40,16 @@ void	print_mutex(t_philo *p, int64_t stamp_us, char *msg)
 
 	stamp_ms = (stamp_us - p->stamp_us_start) / 1000;
 	pthread_mutex_lock(&p->d->mtx_message);
-	printf("op:%5d | %15lld[ms] %d %s", p->d->operation, stamp_ms, p->id, msg);
+	if (INFO == 1)
+	{
+		printf("op:%5d | %15lld[ms] %d %s",
+			p->d->operation, stamp_ms, p->id, msg);
+	}
+	else
+	{
+		printf("%15lld[ms] %d %s",
+			stamp_ms, p->id, msg);
+	}
 	p->d->operation++;
 	pthread_mutex_unlock(&p->d->mtx_message);
 }
